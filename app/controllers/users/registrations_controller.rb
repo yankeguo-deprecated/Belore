13482866ddeef60e06953c11c54aac6155ab14c9
Yanke Guo <me@yanke.io>
2014-12-05 20:14:55 +0800

    ban registration after user created

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,24 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
+  before_filter :configure_sign_up_params, only: [:create]
+  # before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    if User.count == 0
+      super
+    else
+      redirect_to '/'
+    end
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    if User.count == 0
+      super
+    else
+      redirect_to '/'
+    end
+  end
 
   # GET /resource/edit
   # def edit
