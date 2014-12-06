5ab6fd4fb9ea55d8f7b960a7fd229bcd841feb65
Yanke Guo <me@yanke.io>
2014-12-07 05:18:16 +0800

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => ENV['S3_ACCESS_KEY'],         # required
       :aws_secret_access_key  => ENV['S3_ACCESS_SECRET'],      # required
       :region                 => ENV['S3_REGION'],             # optional, defaults to 'us-east-1'
-      :host                   => ENV['S3_HOST']
+      :host                   => ENV['S3_HOST'],
       :endpoint               => ENV['S3_ENDPOINT']            # optional
     }
     config.fog_directory   = 'blog'                            # required
