a9f9aa56ee0bdd90fb4685a7da5bb80c7296a24a
Yanke Guo <me@yanke.io>
2014-12-07 06:11:55 +0800

    update

--- app/models/version.rb
+++ app/models/version.rb
@@ -3,7 +3,7 @@ class Version < ActiveRecord::Base
 
   before_save do
     # Generate content_rich from markdown
-    self.content_rich  = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+    self.content_rich  = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :fenced_code_blocks => true)
     .render(self.content || "")
     # Generate content_plain from content_rich
     self.content_plain = Sanitize.clean(self.content_rich, Sanitize::Config::STRICT)
